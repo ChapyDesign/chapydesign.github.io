2ff797066a734c447b773666a197b3220e4c490d
Chapy <101421121+ChapyDesign@users.noreply.github.com>
2025-11-08 14:11:54 -0800

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".slide");
+  let current = 0;
+
+  function showNextSlide() {
+    slides[current].classList.remove("active");
+    current = (current + 1) % slides.length;
+    slides[current].classList.add("active");
+  }
+
+  setInterval(showNextSlide, 6000);
+});
