fc4e1362269173688c393334f6f6f5bac081e10c
Chapy <101421121+ChapyDesign@users.noreply.github.com>
2025-11-21 21:53:31 -0800

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,11 +2,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
+// Toggle mobile menu
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
 
-// Close menu when clicking a nav link (mobile)
+// Close menu when clicking a nav link
 document.querySelectorAll(".nav-links a").forEach(link => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("open");
